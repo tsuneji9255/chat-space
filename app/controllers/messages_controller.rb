c339a68cad987bb02ceeb8aea801d994819403e7
tsuneji9255 <tsuneharu1024@gmail.com>
2019-12-23 21:36:16 +0900

    Revert "Merge pull request #7 from tsuneji9255/revert-5-sample-chat-space1208"
    This reverts commit 4c54661f3447e567e2c59dcc28ffc98e5c9a1000, reversing
    changes made to 6d8ea0e89b3ab836447a617796cf760b48c24478.

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -9,7 +9,10 @@ class MessagesController < ApplicationController
   def create
     @messages = @group.messages.new(message_params)
     if @messages.save
-      redirect_to group_messages_path(@group), notice: 'メッセージが送信されました'
+      respond_to do |format|
+        format.html { redirect_to group_messages_path(@group), notice: 'メッセージが送信されました' }
+        format.json
+      end
     else
       @messages = @group.messages.includes(:user)
       flash.now[:alert] = 'メッセージを入力してください。'
